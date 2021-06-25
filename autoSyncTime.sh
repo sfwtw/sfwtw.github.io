4964c38cca31d957e8edf1ebcbf73c545aad973a
随疾风 <wtw@cr.cx>
2021-06-25 13:00:31 +0800

    1
    1

--- autoSyncTime.sh
+++
@@ -1,4 +0,0 @@
-ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-if ! crontab -l |grep ntpdate &>/dev/null ; then
-    (echo "* 1 * * * ntpdate ntp1.aliyun.com >/dev/null 2>&1";hwclock -w;crontab -l) |crontab 
-fi
