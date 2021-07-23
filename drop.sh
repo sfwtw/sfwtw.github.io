1ed4393f3b9fe9ec9886915a003c6c0f116831fe
随疾风 <wtw@cr.cx>
2021-07-23 22:50:22 +0800

    Update drop.sh

--- drop.sh
+++ drop.sh
@@ -1,2 +1,12 @@
+cd /etc/
+cat > drop_caches.sh <<EOF
 sync
 echo 3 > /proc/sys/vm/drop_caches #释放PACECACHE、dAccess和INODE
+EOF
+
+
+cat > crontab <<EOF
+*/10 * * * * root /etc/drop_caches.sh
+EOF
+
+service cron restart
